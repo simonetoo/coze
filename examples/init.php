0c0261f66a3f734d6e46ad09bd0740f08f52f003
simonetoo <shihuiqian@xvii.pro>
2025-03-22 15:29:17 +0800

    first commit

--- examples/init.php
+++ examples/init.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Simonetoo\Coze\Concerns\Utils;
 use Simonetoo\Coze\Coze;
+use Simonetoo\Coze\Http\JsonResponse;
 
 $config = [
     'token' => 'pat_e44qxZ0p9VCw0ImW7FLgyE6qWTd7IMGCiKnUAdaqfJKQ3jRkdXeYgEnZSnlGxigL',
@@ -31,7 +32,7 @@ function coze_config(string $key, mixed $default = null): mixed
     return Utils::dataGet($config, $key, $default);
 }
 
-function coze_dump_response($response): void
+function coze_dump_response(JsonResponse $response): void
 {
-    var_dump($response);
+    print json_encode($response->json(), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 }
